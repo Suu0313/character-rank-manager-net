31cd24e3919e893e8b4c21223e21b119dabf0a81
Suu0313 <suuoei@gmail.com>
2025-07-05 11:57:17 +0900

    環境変数修正

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,12 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
-export default defineConfig(() => {
+export default defineConfig(({ mode }) => {
+  // .envファイルから環境変数を読み込む
+  const env = loadEnv(mode, process.cwd(), '');
   return {
     plugins: [react()],
-    base: process.env.VITE_BASE || '/',
+    base: env.VITE_BASE || '/',
   }
 })
